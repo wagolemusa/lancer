e10e30d4cd245674d5d0f0cab5f5d2d283069801
kisumutalent <homiemusa@gmail.com>
2017-12-21 23:44:53 +0300

    Awarded proposal 'in controller update,show and in view shows page

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -22,7 +22,14 @@ end
 def show
 	@gig = Gig.find(params[:id])
 	@proposals = @gig.proposals.order(created_at: :desc)
+	@awarded_proposal = Proposal.where(id: @gig.awarded_proposal).first
 end
+def update
+	@gig = Gig.find(params[:id])
+	@gig.update(gig_params)
+	redirect_to @gig
+end
+
 
 def search
 	@gigs = Gig.search(params).page(params[:page]).per(10)
@@ -32,7 +39,7 @@ end
 
 private
 	def gig_params
-		params.require(:gig).permit(:name, :description, :budget, :location, :open, :category_id, :skill_list)
+		params.require(:gig).permit(:name, :description, :budget, :location, :open, :category_id, :skill_list, :awarded_proposal)
 	end
 
 end
